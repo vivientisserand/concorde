b533fc7e0da34a0c1a69d4c8b0cd07fb67df7769
vivientisserand <79079949+vivientisserand@users.noreply.github.com>
2023-01-11 16:19:20 +0100

    Constructor for dataframe input.

--- struc/graph.py
+++ struc/graph.py
@@ -15,6 +15,20 @@ class Graph():
         """
         self.edges=edges
         self.directed=directed
+        self.nodes = list(set([x[0] for x in edges] + [x[1] for x in edges]))
         
+  @classmethod
+  def from_dataframe(cls, graph_as_df, source=None, target=None, directed=False):
+      """
+      """
+      cols = graph_as_df.columns
+      source, target = source if not None else cols[0], target if not None else cols[1]
+      assert source in cols and target in cols, "Provide valid source and target names (should be in column names)."
+        
+      if not directed:
+          graph_as_df = graph_as_df[graph_as_df[source] < graph_as_df[target]]
+      edges = list(zip(graph_as_df[source], graph_as_df[target]))
+      graph = cls(edges, directed)
+      return graph
     
 
